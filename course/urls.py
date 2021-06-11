6dbaa4b02a7ea0bf8ced23a6af01d39dc558f9a7
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-06-11 13:17:13 +0430

    feat: Add CourseList view

--- course/urls.py
+++ course/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from course.views import ChapterList, ChoiceList, LessonList, QuestionList
+from course.views import ChapterList, ChoiceList, LessonList, QuestionList, CourseList
 
 urlpatterns = [
     path('chapter-list/', ChapterList.as_view()),
     path('choice-list/', ChoiceList.as_view()),
     path('lesson-list/', LessonList.as_view()),
     path('question-list/', QuestionList.as_view()),
+    path('course-list/', CourseList.as_view()),
 ]
--- course/views/__init__.py
+++ course/views/__init__.py
@@ -2,3 +2,4 @@ from .chapter_list import ChapterList
 from .choice_list import ChoiceList
 from .lesson_list import LessonList
 from .question_list import QuestionList
+from .course_list import CourseList
