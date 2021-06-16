31bc756f2d15e74e0bee02a630d0a384f27f2296
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-06-16 21:46:19 +0430

    feat: Link to next page

--- course/models/lesson.py
+++ course/models/lesson.py
@@ -7,3 +7,9 @@ class Lesson(Page):
     name = models.CharField(max_length=100)
     text = models.TextField()
     code_example = models.TextField()
+
+    def next(self):
+        if self.number == self.chapter.lessons.count():
+            return 'http://127.0.0.1:8000/course/question-detail/{}'.format(self.chapter.questions.first().id)
+        return 'http://127.0.0.1:8000/course/lesson-detail/{}'.format(Lesson.objects.get(
+            chapter=self.chapter, number=self.number + 1).id)
--- course/models/question.py
+++ course/models/question.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+from course.models import Chapter
 from course.models.page import Page
 
 
 class Question(Page):
     text = models.TextField()
+
+    def next(self):
+        if self.number == self.chapter.questions.count():
+            if Chapter.objects.count() == self.chapter.id:
+                return 'http://127.0.0.1:8000/course/course-list/'
+            return 'http://127.0.0.1:8000/course/chapter-detail/{}'.format(self.chapter.id + 1)
+        return 'http://127.0.0.1:8000/course/question-detail/{}'.format(Question.objects.get(
+            chapter=self.chapter, number=self.number + 1).id)
