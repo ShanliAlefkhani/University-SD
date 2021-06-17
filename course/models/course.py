dc21efc62dbc82506587d8aac309643b3c8da3b9
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-06-17 21:31:21 +0430

    feat: Add pre description field

--- course/models/course.py
+++ course/models/course.py
@@ -4,4 +4,5 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    pre_description = models.TextField()
     image = models.ImageField(upload_to='course/course-image', null=True)
