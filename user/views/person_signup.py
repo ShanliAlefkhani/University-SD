2a04dad6c23159d6614ec94739d12b699058d4ea
haniye afshar <haniyeafshar06@gmail.com>
2021-06-19 23:53:27 +0430

    feat:Add template person_signup

--- user/views/person_signup.py
+++ user/views/person_signup.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-
+from django.views.generic.list import ListView
 from user.serializers import PersonSerializer
 
 
-class PersonSignUp(generics.CreateAPIView):
+class PersonSignUp(ListView):
     queryset = User.objects.all()
     serializer_class = PersonSerializer
+    template_name = "person_signup_page/person_signup.html"
