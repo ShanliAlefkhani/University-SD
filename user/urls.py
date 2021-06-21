8fd1f7668eb55ee7aac1394aaee0edd1ea150434
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-06-21 22:26:30 +0430

    feat: Add login view and connect

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from user.views import person_signup, Profile, ProfileUpdate
+from user.views import person_signup, Profile, ProfileUpdate, login_user
 
 urlpatterns = [
-    path('', include('rest_framework.urls')),
+    # path('', include('rest_framework.urls')),
     path('person-signup/', person_signup),
     path('profile/<pk>/', Profile.as_view()),
     path('profile-update/<pk>/', ProfileUpdate.as_view()),
+    path('login/', login_user),
 ]
--- user/views/__init__.py
+++ user/views/__init__.py
@@ -1,2 +1,3 @@
 from .person_signup import person_signup
 from .profile import Profile, ProfileUpdate
+from .login import login_user
