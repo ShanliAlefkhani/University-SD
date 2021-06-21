822704790e362565e5f171e487814b6e47299316
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-06-21 12:30:48 +0430

    feat: Migrate company model

--- job/models/application.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from job.models import Job
-from user.models import Person
-
-
-class Application(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
