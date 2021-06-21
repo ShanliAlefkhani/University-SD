dfdd4fb38f6d0a0f50d6316f38f227883ede859c
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-06-21 12:38:36 +0430

    feat: Add JobList view

---
+++ job/views/job_list.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+
+from job.models import Job
+from job.serializers.job import JobSerializer
+
+
+class JobList(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    ordering_fields = ['salary', 'working_hours']
+    search_fields = ['title']
