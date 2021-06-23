f8d987c22b7f1be06f10ae319aee06716c48a560
Mahsima Madanipour <mahsima.mdnp@gmail.com>
2021-06-23 21:59:08 +0430

    feat: Add start page url

--- software_design_project/urls.py
+++ software_design_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('course/', include('course.urls')),
     path('user/', include('user.urls')),
+    path('', TemplateView.as_view(template_name='start_page.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
