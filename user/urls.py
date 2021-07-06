db6a5911eb82f09788848d382776e8b6fc9b2e83
shanlialefkhani <shanli.alefkhani@gmail.com>
2021-07-06 23:24:35 +0430

    feat: Connect person profile update

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from user.views import person_signup, PersonProfile, PersonProfileUpdate, login_user, company_signup, CompanyProfile, \
+from user.views import person_signup, PersonProfile, person_profile_update, login_user, company_signup, CompanyProfile, \
     CompanyProfileUpdate, MainMenu, logout_user
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('person-signup/', person_signup),
     path('company-signup/', company_signup),
     path('person-profile/', PersonProfile.as_view()),
-    path('person-profile-update/', PersonProfileUpdate.as_view()),
+    path('person-profile-update/', person_profile_update),
     path('company-profile/', CompanyProfile.as_view()),
     path('company-profile-update/', CompanyProfileUpdate.as_view()),
     path('login/', login_user),
