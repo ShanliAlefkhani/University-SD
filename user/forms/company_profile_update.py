9cdc3a7137f6217138ab9a4b95830472e2eb89f6
haniye afshar <haniyeafshar06@gmail.com>
2021-07-07 17:21:31 +0430

    feat:Connect company-profile-update

---
+++ user/forms/company_profile_update.py
@@ -0,0 +1,39 @@
+from django import forms
+
+from user.models import Company
+
+class CompanyProfileUpdateForm(forms.Form):
+    name = forms.CharField(
+        widget=forms.TextInput(),
+        label='Company Name',
+    )
+
+    address = forms.CharField(
+        widget=forms.TextInput(),
+        label='Address',
+    )
+
+    creation_date = forms.DateField(
+        widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+        label='Creation Date'
+    )
+
+    about = forms.CharField(
+        widget=forms.Textarea(),
+        label='About',
+    )
+
+    email = forms.CharField(
+        widget=forms.EmailInput(),
+        label='Email',
+    )
+
+    website = forms.CharField(
+        widget=forms.TextInput(),
+        label='Website',
+    )
+
+    telephone_number = forms.IntegerField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Company\'s Telephone Number'}),
+        label='Telephone Number',
+    )
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from user.views import person_signup, PersonProfile, person_profile_update, login_user, company_signup, CompanyProfile, \
-    CompanyProfileUpdate, MainMenu, logout_user
+    company_profile_update, MainMenu, logout_user
 
 urlpatterns = [
     # path('', include('rest_framework.urls')),
@@ -10,7 +10,7 @@ urlpatterns = [
     path('person-profile/', PersonProfile.as_view()),
     path('person-profile-update/', person_profile_update),
     path('company-profile/', CompanyProfile.as_view()),
-    path('company-profile-update/', CompanyProfileUpdate.as_view()),
+    path('company-profile-update/', company_profile_update),
     path('login/', login_user),
     path('main-menu/', MainMenu.as_view()),
     path('logout/', logout_user)
